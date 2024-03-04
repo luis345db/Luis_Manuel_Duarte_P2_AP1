c0e62bf9484a4131252d650efe9e0039be836e58
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-03-04 16:37:43 -0600

    Cree los modelos de accesorio y de Vehiculo

---
+++ Luis_Manuel_Duarte_P2_AP1/Shared/Models/Accesorios.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    internal class Accesorios
+    {
+        [Key] 
+        public string AccesorioId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string? Descripcion { get; set; }
+    }
+}
---
+++ Luis_Manuel_Duarte_P2_AP1/Shared/Models/Vehiculo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class Vehiculo
+    {
+        [Key] 
+        public int VehiculoId { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string? Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El Monto es Requerido")]
+        public float Costo { get; set; }
+
+        [Required(ErrorMessage = "El Monto es Requerido")]
+        public float Monto { get; set; }
+
+        [Required(ErrorMessage = "El Monto es Requerido")]
+        public float Gasto { get; set; }
+
+
+
+    }
+}
