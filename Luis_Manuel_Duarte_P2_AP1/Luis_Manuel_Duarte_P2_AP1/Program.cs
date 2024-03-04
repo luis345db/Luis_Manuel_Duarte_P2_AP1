fbf5b05dc2e736b7b3966a565a3e5c50d4e4a065
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-03-04 17:07:07 -0600

    Cambie el URI

--- Luis_Manuel_Duarte_P2_AP1/Luis_Manuel_Duarte_P2_AP1/Program.cs
+++ Luis_Manuel_Duarte_P2_AP1/Luis_Manuel_Duarte_P2_AP1/Program.cs
@@ -1,11 +1,9 @@
-using Luis_Manuel_Duarte_P2_AP1;
+using Luis_Manuel_Duarte_P2.Assembly;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5005") });
 await builder.Build().RunAsync();
