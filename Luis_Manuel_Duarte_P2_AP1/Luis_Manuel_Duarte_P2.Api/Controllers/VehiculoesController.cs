9c33bc3547471d422cb6d46bc9db58a9f5e4d53f
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-03-04 17:30:47 -0600

    Cree el controller de Vehiculo

---
+++ Luis_Manuel_Duarte_P2_AP1/Luis_Manuel_Duarte_P2.Api/Controllers/VehiculoesController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Luis_Manuel_Duarte_P2.Api.DAL;
+using Shared.Models;
+
+namespace Luis_Manuel_Duarte_P2.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehiculoesController : ControllerBase
+    {
+        private readonly Contexto _context;
+
+        public VehiculoesController(Contexto context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Vehiculoes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Vehiculo>>> GetVehiculo()
+        {
+            return await _context.Vehiculo.ToListAsync();
+        }
+
+        // GET: api/Vehiculoes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Vehiculo>> GetVehiculo(int id)
+        {
+            var vehiculo = await _context.Vehiculo.FindAsync(id);
+
+            if (vehiculo == null)
+            {
+                return NotFound();
+            }
+
+            return vehiculo;
+        }
+
+        // PUT: api/Vehiculoes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutVehiculo(int id, Vehiculo vehiculo)
+        {
+            if (id != vehiculo.VehiculoId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(vehiculo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VehiculoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Vehiculoes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Vehiculo>> PostVehiculo(Vehiculo vehiculo)
+        {
+            _context.Vehiculo.Add(vehiculo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetVehiculo", new { id = vehiculo.VehiculoId }, vehiculo);
+        }
+
+        // DELETE: api/Vehiculoes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVehiculo(int id)
+        {
+            var vehiculo = await _context.Vehiculo.FindAsync(id);
+            if (vehiculo == null)
+            {
+                return NotFound();
+            }
+
+            _context.Vehiculo.Remove(vehiculo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool VehiculoExists(int id)
+        {
+            return _context.Vehiculo.Any(e => e.VehiculoId == id);
+        }
+    }
+}
