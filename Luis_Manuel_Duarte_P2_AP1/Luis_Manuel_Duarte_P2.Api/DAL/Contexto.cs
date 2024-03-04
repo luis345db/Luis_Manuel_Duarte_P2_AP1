755ef256a2874ad186a657badd038ef19df5a619
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-03-04 16:59:34 -0600

    Cree el contexto

---
+++ Luis_Manuel_Duarte_P2_AP1/Luis_Manuel_Duarte_P2.Api/DAL/Contexto.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Models;
+
+namespace Luis_Manuel_Duarte_P2.Api.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+        public DbSet<Vehiculo> Vehiculo { get; set; }
+        public DbSet<Accesorios> Accesorios { get; set; }
+        public DbSet<Vehiculos_Detalle> Vehiculos_Detalle { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Accesorios>().HasData(new List<Accesorios>()
+            {
+                new Accesorios() { AccesorioId = 1, Descripcion = "Camara trasera" },
+                new Accesorios() { AccesorioId = 2, Descripcion = "Pantalla interior" },
+                new Accesorios() { AccesorioId = 3, Descripcion = "Interior en piel" },
+                new Accesorios() { AccesorioId = 4, Descripcion = "Sun Roof" },
+                new Accesorios() { AccesorioId = 5, Descripcion = "Aros de Lujo" }
+            });
+        }
+    }
+}
