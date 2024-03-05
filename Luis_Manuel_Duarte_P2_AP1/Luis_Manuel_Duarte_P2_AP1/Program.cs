87c0ef3e52e9659d76ae9e02d5b9991c614d2fd5
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-03-04 19:21:44 -0600

    Solucione problemas De URI

--- Luis_Manuel_Duarte_P2_AP1/Luis_Manuel_Duarte_P2_AP1/Program.cs
+++ Luis_Manuel_Duarte_P2_AP1/Luis_Manuel_Duarte_P2_AP1/Program.cs
@@ -5,5 +5,5 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7040") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7234/") });
 await builder.Build().RunAsync();
